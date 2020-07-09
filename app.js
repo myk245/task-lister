b320add78c39a1f67c44bb5f016fb8bcc6c8f537
myk245 <myk245@nyu.edu>
2020-07-09 01:14:28 -0400

    add functionality to delete, clear, and filter tasks

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const taskInput = document.querySelector('#task');
 // load all event listeners
 loadEventListeners = () => {
    // add task event
-   form.addEventListener('submit', addTask)
+   form.addEventListener('submit', addTask);
+   // remove task event
+   taskList.addEventListener('click', removeTask);
+   // clear tasks event
+   clearButton.addEventListener('click', clearTasks);
+   // filter tasks event
+   filter.addEventListener('keyup', filterTasks); 
 }
 
 addTask = (event) => {
@@ -31,6 +37,34 @@ addTask = (event) => {
    taskInput.value = '';
 
    event.preventDefault(); 
+}
+
+removeTask = (event) => {
+   if (event.target.parentElement.classList.contains('delete-item')) {
+      if (confirm('Are you sure?')) {
+         event.target.parentElement.parentElement.remove();   
+      }
+   }
+}
+
+clearTasks = () => {
+   // while there is still a first child element, remove it
+   while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild)
+   }
+}
+
+filterTasks = (event) => {
+   const text = event.target.value.toLowerCase(); 
+   document.querySelectorAll('.collection-item').forEach(function (task) {
+      const item = task.firstChild.textContent; 
+      // if there is no match, the index of text will equal -1
+      if (item.toLowerCase().indexOf(text) != -1) {
+         task.style.display = 'block'; 
+      } else {
+         task.style.display = 'none'; 
+      }
+   });
 }
 
 // call function to load all event listeners 
